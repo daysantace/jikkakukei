cb072d92d93e2dcaed619ef2615d285d565987c4
daysantace <daysant@proton.me>
2024-07-31 12:52:12 +0100

    organisation

---
+++ source/README.md
@@ -0,0 +1,9 @@
+# Jikkakukei source
+
+This directory is the raw source of Jikkakukei and the DGFS. It contains all the documentation and comments, with
+
+The `/dist` directory is the source of Jikkakukei and DGFS that is distributed (and installed via `install.lua`). It is stripped of all comments and whitespace to save space.
+
+## Further information
+
+Refer to `doc`.
--- source/boot/dgfs.lua
+++ source/boot/dgfs.lua
@@ -1,5 +1,5 @@
--- io.lua
--- API to read and write to the DFS
+-- dgfs.lua
+-- API to read and write to the DGFS
 
 -- Copyleft daysant, Prellit, Everytab - Jikkakukei
 -- This file is licensed under the terms of the Affero GPL v3.0-or-later.
@@ -12,9 +12,6 @@ local boot = component.proxy(component.list("filesystem")())
 -------------------------------------------------------------------------------
 
 initialized_fat = main.readByte(1)
-
-function fat_update(sector,location,owner,_reserved,name)
-end
 
 if initialized_fat == 1 then
     goto load_fat -- FAT is initialized, skip
